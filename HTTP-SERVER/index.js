4b9c79cb4a8e4d935efb965a793b2935d2865ac4
Ritwik <ritwikhero@gmail.com>
2026-01-17 18:11:24 +0530

    submodules deleted

---
+++ HTTP-SERVER/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/conversation", (req, res) => {
+  console.log(req.body);
+  res.send({
+    msg: "ka re kaisan ba",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
