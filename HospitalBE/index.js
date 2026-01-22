a8ab3c77b2be59630576dc2c27e85c64a9a0e151
Ritwik <ritwikhero@gmail.com>
2026-01-23 01:11:25 +0530

    hospital BE project

---
+++ HospitalBE/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+const users = [
+  {
+    fullName: "rohit",
+    kidney: [
+      {
+        healthy: true,
+      },
+    ],
+  },
+];
+
+app.get("/", (req, res) => {
+  const kidney = users[0].kidney;
+  console.log(kidney);
+  const noOfKidney = kidney.length;
+  console.log(noOfKidney);
+
+  const healthyKidney = kidney.filter((kidney) => kidney.healthy === true);
+  const noOfHealthyKidney = healthyKidney.length;
+
+  const unhealthyKidney = noOfKidney - noOfHealthyKidney;
+
+  res.json({ noOfKidney, noOfHealthyKidney, unhealthyKidney });
+});
+
+app.post("/", (req, res) => {
+  const isHealthy = req.body.isHealthy;
+
+  users[0].kidney.push({
+    healthy: isHealthy,
+  });
+
+  res.json({ message: "done" });
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
