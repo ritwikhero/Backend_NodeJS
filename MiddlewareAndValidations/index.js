36a094ac18de60514759ddfa2222def3b12894b8
Ritwik <ritwikhero@gmail.com>
2026-02-16 01:15:35 +0530

    zod validation started

--- MiddlewareAndValidations/index.js
+++ MiddlewareAndValidations/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const zod = require("zod");
 const app = express();
 const PORT = 3000;
+// const schema = zod.array(zod.number());
+
+const schema = zod.object({
+  email: zod.email(),
+  password: zod.password(),
+  kidneys: zod.array(zod.number()),
+  country: zod.literal("IN").or(zod.literal("US")),
+});
 
 app.use(express.json());
 
 app.post("/health-checkup", (req, res) => {
   const kidneys = req.body.kidneys;
-  const kidneyLength = kidneys.length;
+  const response = schema.safeParse(kidneys);
+  // const kidneyLength = kidneys.length;
 
-  return res.send("You have " + kidneyLength + " kidneys");
+  // return res.send("You have " + kidneyLength + " kidneys");
+  return res.send({ response });
 });
 
 app.listen(PORT, () => {
