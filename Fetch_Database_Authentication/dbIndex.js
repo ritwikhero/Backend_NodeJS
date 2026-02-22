020fccab28f0f83606fdb240928491b8fce4d78e
Ritwik <ritwikhero@gmail.com>
2026-02-23 01:40:11 +0530

    few pointers left for db integration

---
+++ Fetch_Database_Authentication/dbIndex.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const jwtPassword = "123456";
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.zqnhm2y.mongodb.net/user-app?retryWrites=true&w=majority`,
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const User = mongoose.model("User", {
+  name: String,
+  username: String,
+  password: String,
+});
+
+async function userExists(username, password) {
+  // should check in the database
+  const userExist = await User.findOne({
+    username: username,
+    password: password,
+  });
+  return userExist;
+}
+
+app.post("/signin", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const exsistingUser = await userExists(username, password);
+
+  if (exsistingUser) {
+    return res.status(403).json({
+      msg: "User already exists in our database",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // return a list of users other than this username from the database
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000);
