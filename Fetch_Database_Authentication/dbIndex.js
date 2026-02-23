ae1958fd1bdcc2d46dae217fc69d9541866188e9
Ritwik <ritwikhero@gmail.com>
2026-02-24 01:25:23 +0530

    db video completed new assingment started

--- Fetch_Database_Authentication/dbIndex.js
+++ Fetch_Database_Authentication/dbIndex.js
@@ -41,18 +41,28 @@ app.post("/signin", async function (req, res) {
     });
   }
 
+  const user = new User({
+    name: req.body.name,
+    username: req.body.username,
+    password: req.body.password,
+  });
+  await user.save();
+
   var token = jwt.sign({ username: username }, jwtPassword);
   return res.json({
     token,
+    msg: "User created successfully",
   });
 });
 
-app.get("/users", function (req, res) {
+app.get("/users", async function (req, res) {
   const token = req.headers.authorization;
   try {
     const decoded = jwt.verify(token, jwtPassword);
     const username = decoded.username;
     // return a list of users other than this username from the database
+    const filteredUsers = await User.find({ username: { $ne: username } });
+    res.json({ users: filteredUsers });
   } catch (err) {
     return res.status(403).json({
       msg: "Invalid token",
