b05f55afa99b81609a97c76524f202cce0454997
Ritwik <ritwikhero@gmail.com>
2026-03-17 01:55:24 +0530

    userMiddleware added

--- Paytm/Backend/middleware/user.js
+++ Paytm/Backend/middleware/user.js
@@ -3,3 +3,27 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 const jwtPassword = process.env.JWT_SECRET;
+
+async function userMiddleware(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(403).json({
+        message: "Invalid authorization format",
+      });
+    }
+
+    const token = authorization.split("")[1];
+    const decode = jwt.verify(token, jwtPassword);
+
+    req.userId = decode.userId;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      message: "User doesnt exist",
+    });
+  }
+}
+
+module.exports = userMiddleware;
