6d8776af5c8072c15c7d96297164db04c0fb0aa3
Ritwik <ritwikhero@gmail.com>
2026-04-01 01:40:47 +0530

    added balance route

--- Paytm/Backend/index.js
+++ Paytm/Backend/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const userRouter = require("./routes/user");
+const accountRouter = require("./routes/account");
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
 app.use("/api/user", userRouter);
-// app.use("/api", require("/routes/user"));
+app.use("/api/account", accountRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ Paytm/Backend/routes/account.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+const { User, Account } = require("../db/db");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const zod = require("zod");
+dotenv.config();
+const jwtPassword = process.env.JWT_SECRET;
+
+router.get("/balance", userMiddleware, async (req, res) => {
+  try {
+    const account = await Account.findOne({
+      userId: req.userId,
+    });
+
+    if (!account) {
+      return res.status(404).json({
+        message: "Account not found",
+      });
+    }
+
+    return res.status(200).json({
+      balance: account.balance,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+
+module.exports = router;
